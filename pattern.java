1eb45690e4a3f882dc48626b4da9fd9fb9bded3f
KARU VENKATA KRISHNA <150501282+krishnakaru18@users.noreply.github.com>
2025-08-05 22:06:57 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -7,7 +7,7 @@ public class pattern {
 		System.out.println("Pattern-1");
 		for (int i=1; i<=5; i++) {
 			for (int j=1; j<=5; j++) {
-				System.out.print("*");
+				System.out.print("* ");
 			}
 			System.out.println();
 		}
@@ -49,6 +49,74 @@ public class pattern {
 				}
 				count++;						
 			}					
+			System.out.println();
+		}
+		//Pattern-5
+		System.out.println("Pattern-5");
+		for (int i=1; i<=5; i++) {
+			for (int j=1; j<=5; j++) {
+				if (i*j<10) {
+					System.out.print("0"+i*j+" ");
+				}
+				else {
+					System.out.print(i*j+" ");
+				}
+			}
+			System.out.println();
+		}		
+		
+		//Pattern-6
+		System.out.println("Pattern-6");
+		for (int i=1; i<=5; i++) {
+			for (int j=1; j<=5; j++) {
+					System.out.print(i+j-1+" ");
+			}
+			System.out.println();
+		}
+		
+		//Pattern-7
+		System.out.println("Pattern-7");
+		for (int i=1; i<=5; i++) {
+			for (int j=1; j<=i; j++) {
+					System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		//Pattern-8
+		System.out.println("Pattern-8");
+		for (int i=1; i<=5; i++) {
+			for (int j=1; j<=i; j++) {
+					System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		
+		//Pattern-9
+		System.out.println("Pattern-9");
+		for (int i=1; i<=5; i++) {
+			//Printing Spaces
+			for (int j=i; j<=5; j++) {
+				System.out.print(" ");
+			}
+			//Printing Stars
+			for (int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		//Pattern-10
+		System.out.println("Pattern-10");
+		for (int i=1; i<=5; i++) {
+			//Printing Spaces
+			for (int j=i; j<=5; j++) {
+				System.out.print(" ");
+			}
+			//Printing Stars
+			for (int j=1; j<=i; j++) {
+				System.out.print("* ");
+			}
 			System.out.println();
 		}
 	}
